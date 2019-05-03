caccb001e452f24de2272c69e711b8051775be4c
Roque rbugadev@gmail.com <rbugadev@gmail.com>
2019-05-03 16:02:51 +0200

    fisrt commit

---
+++ src/main/java/com/formation/service/IArticleService.java
@@ -0,0 +1,17 @@
+package com.formation.service;
+
+import java.util.List;
+
+import com.formation.entity.Article;
+
+public interface IArticleService {
+	List<Article> getAllArticles();
+
+	Article getArticleById(int articleId);
+
+	boolean addArticle(Article article);
+
+	void updateArticle(Article article);
+
+	void deleteArticle(int articleId);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Dumping data for table ecom.articles: ~3 rows (approximately)
+INSERT INTO `articles` (`article_id`, `title`, `category`) VALUES
+	(1, 'Java Concurrency', 'Java'),
+	(2, 'Hibernate HQL ', 'Hibernate'),
+	(3, 'Spring MVC with Hibernate', 'Spring'); 
